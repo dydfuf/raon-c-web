f741d1973a7bed56eca4c47e2c6e2c21c2f8c69d
Raon <41016331+dydfuf@users.noreply.github.com>
2024-11-24 02:19:52 +0900

    supabase

---
+++ app/post/actions.ts
@@ -0,0 +1,11 @@
+"use server";
+
+import { createClient } from "@/lib/supabase/server";
+
+export async function getPosts() {}
+
+export async function getPost() {}
+
+export async function createPost() {}
+
+export async function updatePost() {}
